70b01779c981bcad15aff8e98ea44fa3c5a04234
miguel <miguel.garciabohigues@hotmail.com>
2023-06-29 15:25:50 +0200

    fixes to monte carlo control implementation

--- monte_carlo_control.py
+++ monte_carlo_control.py
@@ -4,28 +4,25 @@ import numpy as np
 import random
 
 N_0 = 100
-N_s = {}
-N_sa = {}
-Q_sa = {}
+N_sa = np.zeros((11,22,2))
+Q_sa = np.zeros((11,22,2))
 actions = [env.Actions.HIT, env.Actions.STICK]
 
 def get_epsilon(state):
-    n_st = N_s.get(state, 0)
+    n_st = np.sum(N_sa[state.broker_hand][state.player_hand])
     return N_0/(N_0 + n_st)
 
 def get_alpha(state, action):
-    n_sta = N_sa.get((state, action), 0)
+    n_sta = N_sa[state.broker_hand][state.player_hand][int(action)]
     return 1/n_sta
 
 def update_N(state, action):
-    n_st = N_s.get(state, 0)
-    n_stat = N_sa.get((state, action), 0)
-    N_s[state] = n_st + 1
-    N_sa[(state, action)] = n_stat + 1
+    #N_sa[state.broker_hand][state.player_hand] += 1
+    N_sa[state.broker_hand][state.player_hand][int(action)] += 1
 
 def update_q_value(state, action, reward):
-    q_stat = Q_sa.get((state, action), 0)
-    Q_sa[(state, action)] = q_stat + get_alpha(state, action) * (reward - q_stat)
+    q_stat = Q_sa[state.broker_hand][state.player_hand][int(action)]
+    Q_sa[state.broker_hand][state.player_hand][int(action)] += get_alpha(state, action) * (reward - q_stat)
 
 def select_action(state):
     eps = get_epsilon(state)
@@ -39,7 +36,7 @@ def get_argmax_q_value(state):
     max = -10000
     argmax = None
     for action in actions:
-        local_max = Q_sa.get((state, action),0)
+        local_max = Q_sa[state.broker_hand][state.player_hand][int(action)]
         if  local_max > max:
             max = local_max
             argmax = action
@@ -49,22 +46,17 @@ def monte_carlo_control(num_episodes):
     i = 0
     environment = env.Easy21Environment()
     while i < num_episodes:
-        #print('EPISODE: {}'.format(i))
         state_action_pair = []
         rewards = []
         st = env.State(environment.draw(black_only=True), environment.draw(black_only=True))
-        # print('INITIAL STATE:\n{}\n'.format(st))
         while not st.is_terminal:
             act = select_action(st)
 
             st_, reward = environment.step(st, act)
             
-            #print('Action chosen:{}\nReward:{}\nNew state:{}\n\n'.format(act, reward, st_))
-
             rewards.append(reward)
             state_action_pair.append((st, act))
             st = st_
-        # print('WIN' if reward > 0 else 'DEFEAT')
 
         #update N and Q
         for j, state_action in enumerate(state_action_pair):
@@ -72,26 +64,30 @@ def monte_carlo_control(num_episodes):
             action = state_action[1]
             update_N(state, action)
             update_q_value(state, action, rewards[j])
-        # print(Q_sa)
-        # print()
-        # print()
-        # print('----------------------------------')
         i+=1
 
 def plot_q():
-    mat_3d = []
-    for key in Q_sa.keys():
-        state = key[0]
-        mat_3d.append([state.broker_hand, state.player_hand, Q_sa[key]])
-    mat_3d = np.array(mat_3d)
     
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    ax.plot_trisurf(mat_3d[:,0], mat_3d[:,1], mat_3d[:,2], linewidth=0, antialiased=False,
-                    cmap='viridis', edgecolor='none')
+
+    aux = np.zeros((11,22))
+    for i in range(1,11):
+        for j in range(1,22):
+            aux[i,j] = max(Q_sa[i,j,:])
+    aux = aux[1:, 1:]
+
+    x = [i for i in range(1,11)]
+    y = [i for i in range(1,22)]
+
+    x, y = np.meshgrid(x, y) 
+
+    aux = aux.transpose()
+    ax.plot_surface(x, y, aux, rstride=1, cstride=1,
+                    cmap='viridis')
     ax.set_title('easy 21')
     plt.savefig('MC_control')
 
 if __name__ == '__main__':
-    monte_carlo_control(5000)
+    monte_carlo_control(5000000)
     plot_q()
